d92bc3201a2969d3e6883c29be3342cd12ac133c
econDR <34520011+econDR@users.noreply.github.com>
2025-04-22 12:22:12 +0000

    Deploying to gh-pages from @ econDR/domenico.rossignoli.github.io@fd9fa9378f3ad85bc435d535da32a26d90ce65d6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-seminars-amp-conferences",
           title: "seminars &amp; conferences",
           description: "",
